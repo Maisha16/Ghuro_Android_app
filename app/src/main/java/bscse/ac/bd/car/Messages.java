8e24507913da5773c6b7b90838695ce7c17c266b
Maisha Farzana <swarna1614@gmail.com>
2019-05-02 01:59:27 +0600

    message

--- app/src/main/java/bscse/ac/bd/car/Messages.java
+++ app/src/main/java/bscse/ac/bd/car/Messages.java
@@ -1,4 +1,28 @@
 package bscse.ac.bd.car;
 
 public class Messages {
+    private String name;
+    private String message;
+
+
+    public Messages(String name,String message){
+        this.message=message;
+        this.name=name;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
